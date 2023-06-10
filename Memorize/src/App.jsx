3d367e38022c7fbd05457fb46e0dd7d47d1c1378
Holme.A <homer.asava@gmail.com>
2023-06-11 02:19:22 +0700

    draft v.1

--- Memorize/src/App.jsx
+++ Memorize/src/App.jsx
@@ -1,9 +1,13 @@
 import { useEffect, useReducer, createContext } from "react";
-import TodoList from "./ToDoCard";
+import ToDoCard from "./ToDoCard";
+import CreateForm from "./creatingMemoComponent";
 import { RiStickyNoteFill } from "react-icons/ri";
 
-const UPDATE_MEMO = "UPDATE_MEMO";
-const UPDATE_MEMO_ITEM = "UPDATE_MEMO_ITEM";
+const SET_MEMO = "SET_MEMO";
+const UPDATE_MEMO_ITEMS = "UPDATE_MEMO_ITEMS";
+const DELETE_MEMO_ITEM = "DELETE_MEMO_ITEM";
+const DELETE_MEMO = "DELETE_MEMO";
+const ADD_MEMO = "ADD_MEMO";
 const BASED_URL = "http://localhost:8000/";
 const MEMO_URL = BASED_URL + "memo/";
 let MEMO_ITEM_URL = (memo_id, item_id) => {
@@ -16,30 +20,47 @@ const MemoContext = createContext();
 let initialState = { memos: [] };
 let reducer = (state, action) => {
   switch (action.type) {
-    case UPDATE_MEMO:
+    case ADD_MEMO:
+      return {
+        ...state,
+        memos: [...state.memos, action.payload],
+      };
+    case SET_MEMO:
       return {
         ...state,
         memos: action.payload,
       };
-    // return {
-    //   ...state,
-    //   todos: state.todos.map((todo) => {
-    //     if (todo.id === action.payload.todoId) {
-    //       return {
-    //         ...todo,
-    //         items: todo.items.filter(
-    //           (item) => item.id !== action.payload.itemId
-    //         ),
-    //       };
-    //     }
-    //     return todo;
-    //   }),
-    // };
-    case UPDATE_MEMO_ITEM:
+    case UPDATE_MEMO_ITEMS:
       return {
         ...state,
 
-        memos: action.payload,
+        memos: state.memos.map((memo) => {
+          if (memo.id === action.payload.memo_id) {
+            return { ...memo, items: action.payload.updated_items };
+          }
+
+          return memo;
+        }),
+      };
+    case DELETE_MEMO_ITEM:
+      return {
+        ...state,
+
+        memos: state.memos.map((memo) => {
+          if (memo.id === action.payload.memo_id) {
+            const updatedItems = memo.items.filter(
+              (item) => item.id !== action.payload.memoItemId
+            );
+            return { ...memo, items: updatedItems };
+          }
+
+          return memo;
+        }),
+      };
+    case DELETE_MEMO:
+      return {
+        ...state,
+        memos: state.memos.filter((memo) => memo.id !== action.payload.memo_id),
       };
     default:
       return state;
@@ -53,7 +74,7 @@ function App() {
       try {
         const response = await fetch(MEMO_URL);
         const json = await response.json();
-        dispatchMemo({ type: UPDATE_MEMO, payload: json });
+        dispatchMemo({ type: SET_MEMO, payload: json });
       } catch (error) {
         console.error(error);
       }
@@ -63,7 +84,7 @@ function App() {
   }, []);
 
   return (
-    <MemoContext.Provider value={dispatchMemo}>
+    <MemoContext.Provider value={{ memoState, dispatchMemo }}>
       <div className="bg-slate-200 flex-col min-w-sm max-w-6xl m-auto items-center text-center justify-center  h-full min-h-screen">
         <li className="inline-flex text-5xl gab-5 p-10 font-extrabold   text-emerald-400  ">
           <h1 className=" text-transparent bg-clip-text  bg-gradient-to-r  from-slate-600 to-emerald-500">
@@ -74,12 +95,25 @@ function App() {
 
         <div className=" flex  flex-wrap justify-center g-2 font-sans">
           {memoState.memos.map((memo) => (
-            <ToDoCard key={memo.id} a={memo} memo={memo} />
+            <ToDoCard key={memo.id} memo={memo} />
           ))}
+        </div>
+        <div className=" flex  flex-wrap justify-center g-2 font-sans"></div>
+        <div>
+          <CreateForm />
         </div>
       </div>
     </MemoContext.Provider>
   );
 }
-export { MemoContext, MEMO_URL, MEMO_ITEM_URL };
+export {
+  MemoContext,
+  MEMO_URL,
+  BASED_URL,
+  UPDATE_MEMO_ITEMS,
+  DELETE_MEMO_ITEM,
+  MEMO_ITEM_URL,
+  ADD_MEMO,
+  DELETE_MEMO,
+};
 export default App;
--- Memorize/src/ToDoCard.jsx
+++ Memorize/src/ToDoCard.jsx
@@ -1,50 +1,112 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useReducer, useState, useRef } from "react";
 import { TiDelete } from "react-icons/ti";
 import { MdCleaningServices } from "react-icons/md";
+import { RiDeleteBin2Fill } from "react-icons/ri";
 import { MEMO_URL, MemoContext } from "./App";
-import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+import {
+  BASED_URL,
+  UPDATE_MEMO_ITEMS,
+  MEMO_ITEM_URL,
+  DELETE_MEMO_ITEM,
+  DELETE_MEMO,
+} from "./App";
+const makeApiRequest = async (url, method, data) => {
+  try {
+    const requestOptions = {
+      method: method,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+    if (data) {
+      requestOptions.body = JSON.stringify(data);
+    }
+    const response = await fetch(url, requestOptions);
+    if (response.ok) {
+      if (method === "DELETE") {
+        console.log("?");
+        return;
+      }
 
+      return await response.json();
+    } else {
+      throw new Error("Request failed");
+    }
+  } catch (error) {
+    console.error(error);
+    // Handle the error
+  }
+};
 const ToDoCard = (props) => {
+  const dragRefId = useRef(null);
+  const dragOverRef = useRef(null);
+
   const [inputValue, setInputValue] = useState("");
   const { memoState, dispatchMemo } = useContext(MemoContext);
   const handleInputChange = (e) => {
     setInputValue(e.target.value);
   };
 
-  const addItem = () => {};
-  const deleteItem = (index) => {};
-  const setCompleteStatus = (index) => {};
+  const updateMemoItemsStatus = async (target_id, data) => {
+    const url = MEMO_ITEM_URL(props.memo.id, target_id);
+    return await makeApiRequest(url, "PATCH", data);
+  };
+  const updateMemoItems = async (data) => {
+    const url = MEMO_ITEM_URL(props.memo.id, dragRefId.current);
+    return await makeApiRequest(url, "PATCH", data);
+  };
+
+  const addNewMemoItem = async (data) => {
+    const url = MEMO_ITEM_URL(props.memo.id);
+    return await makeApiRequest(url, "POST", data);
+  };
+  const deleteMemo = async () => {
+    const url = MEMO_URL + props.memo.id + "/";
+    return await makeApiRequest(url, "DELETE");
+  };
+  const deleteMemoItem = async (target_id) => {
+    const url = MEMO_ITEM_URL(props.memo.id, target_id);
+    return await makeApiRequest(url, "DELETE");
+  };
+  const handleMemoUpdate = (updatedItems) => {
+    dispatchMemo({
+      type: UPDATE_MEMO_ITEMS,
+      payload: { memo_id: props.memo.id, updated_items: updatedItems },
+    });
+  };
+
+  const sortAndUpdateMemoItem = async (event) => {
+    const data = { order: dragOverRef.current };
+    const updatedItems = await updateMemoItems(data);
+    handleMemoUpdate(updatedItems);
+  };
+  const handleDeleteMemo = async () => {
+    const updatedItems = await deleteMemo();
+    dispatchMemo({
+      type: DELETE_MEMO,
+      payload: { memo_id: props.memo.id },
+    });
+  };
+  const deleteItem = async (memoItemId) => {
+    const updatedItems = await deleteMemoItem(memoItemId);
+    dispatchMemo({
+      type: DELETE_MEMO_ITEM,
+      payload: { memo_id: props.memo.id, memoItemId: memoItemId },
+    });
+  };
   const handleAddItem = async (e) => {
     e.preventDefault();
-    const data = {
-      name: e.target.inputValue,
-      reminder_at: "2023-06-10",
-    };
-    try {
-      const response = await fetch(MEMO_URL, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(data),
-      });
-
-      if (response.ok) {
-        const data = await response.json();
-        console.log(data); // Handle the response as needed
-        // You can update the state or fetch the updated list of items here
-
-        // Reset the form input
-      } else {
-        throw new Error("Request failed");
-      }
-    } catch (error) {
-      console.error(error);
-      // Handle the error
-    }
+    const data = { description: inputValue };
+    const updatedItems = await addNewMemoItem(data);
+    handleMemoUpdate(updatedItems);
+    setInputValue("");
+  };
+  const handleCompletedStatus = async (memoItem) => {
+    const data = { is_completed: !memoItem.is_completed };
+    const updatedItems = await updateMemoItemsStatus(memoItem.id, data);
+    handleMemoUpdate(updatedItems);
   };
   return (
-    // {console.log(key)}
     <div className=" m-5 w-3/6 max-w-sm  h-fit bg-white  rounded-3xl drop-shadow-sm">
       <div
         className=" sh
@@ -52,9 +114,8 @@ const ToDoCard = (props) => {
         bg-slate-100"
       >
         <h1 className="ml-5 text-lg inline-flex items-baseline gap-3">
-          {props.memo.name}({props.memo.reminder_at})
-          <MdCleaningServices />
-          <TiDelete />
+          {props.memo.name}
+          <TiDelete onClick={() => handleDeleteMemo(props.memo.id)} />
         </h1>
         {props.memo.items.length > 0 ? (
           <h2 className="ml-10 font-light italic">
@@ -64,21 +125,47 @@ const ToDoCard = (props) => {
       </div>
       <div>
         <ul className=" ml-10 text-left">
-          {props.memo.items.map((item) => (
+          {props.memo.items.map((item, index) => (
             <div
               key={item.order}
-              className="ml-2 flex-none flex-col align-text-bottom "
-              draggable={true}
+              className="ml-2  flex align-text-bottom   items-baseline  m-3 gap-2 text-lg "
+              draggable
+              onDragStart={(e) => {
+                dragRefId.current = item.id;
+              }}
+              onDragEnter={(e) => {
+                dragOverRef.current = index + 1;
+              }}
+              onDragEnd={sortAndUpdateMemoItem}
+              onDragOver={(e) => e.preventDefault()}
             >
-              <h1>{item.description}</h1>
-              {/* <TiDelete onClick={() => deleteTodo(item.order)} /> */}
+              <input
+                type="checkbox"
+                id={item.id}
+                className="w-4 h-4 bg-gray-100 border-gray-300 rounde "
+                value="adsf"
+                checked={item.is_completed}
+                onChange={() => handleCompletedStatus(item)}
+              ></input>
+              <h1
+                className={` ${
+                  item.is_completed ? "line-through" : "no-underline"
+                }`}
+              >
+                {item.description}
+              </h1>
+              <RiDeleteBin2Fill
+                onClick={() => deleteItem(item.id)}
+                className="text-slate-500"
+              />
             </div>
           ))}
         </ul>
       </div>
-      <div className=" rounded-3xl  bg-slate-300">
+      <div className=" rounded-b-3xl  bg-gradient-to-br from-slate-200 to-emerald-300">
         <input
-          className=" bg-transparent"
+          className=" bg-transparent "
+          placeholder="create new task"
           type="text"
           value={inputValue}
           onChange={handleInputChange}
@@ -88,5 +175,5 @@ const ToDoCard = (props) => {
     </div>
   );
 };
-
+export { makeApiRequest };
 export default ToDoCard;
